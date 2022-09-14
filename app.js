7153ba304c41942ebc569ae60c2f120065f93a6e
Jacob Doherty <doherty.jacob.p@gmail.com>
2022-09-13 17:20:04 -0700

    add DOM elements to app.js and gameState variables also adds a few styles in pokemon.css

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 /* Imports */
 
 /* State */
+let gameState = 'choose';
+let choices = '';
+let compChoices = '';
+let result = ;
+
+let battles = 0;
+let wins = 0;
 
 /* Actions */
 function loadPage() {}
 
 /* Components */
+
+// Scoreboard
+const winsDisplay = document.getElementById('wins-display');
+const totalDisplay = document.getElementById('total-display');
+const lossesDisplay = document.getElementById('losses-display');
+
 
 /* Component */
 // get DOM
