66b997104caa5eeca52e3d173693ad0e2169640f
Jacob Doherty <doherty.jacob.p@gmail.com>
2022-09-18 00:17:13 -0700

    working game logic! and working scoreboard! add some css too and play again

--- app.js
+++ app.js
@@ -1,18 +1,70 @@
 /* Imports */
 
+import { getRandomItem } from './utils.js';
+
 /* State */
 let gameState = 'choose'; //'choose', 'result'
-let userchoice = ''; //charizard, blastoise, venusaur
+let trainerChoice = ''; //charizard, blastoise, venusaur
 let compChoice = ''; //charizard, blastoise, venusaur
-let result = '';
 
 let battles = 0;
 let wins = 0;
+let losses = 0;
+let draws = 0;
+
+// probability array
+const pokeChoices = ['charizard', 'blastoise', 'venusaur'];
 
 /* Actions */
 function loadPage() {
     displayChoices();
+    displayResults();
 }
+
+function choosePokemon(userChoice) {
+    gameState = 'results';
+    battles++;
+    trainerChoice = userChoice;
+    compChoice = getRandomItem(pokeChoices);
+    selectWinner(trainerChoice, compChoice);
+    totalDisplay.textContent = battles;
+}
+
+const selectWinner = (trainerChoice, compChoice) => {
+    let result = '';
+
+    if (trainerChoice === compChoice) {
+        result = 'draw';
+        draws++;
+        drawsDisplay.textContent = draws;
+    } else if (trainerChoice === 'charizard' && compChoice === 'venusaur') {
+        result = 'win';
+        wins++;
+        winsDisplay.textContent = wins;
+    } else if (trainerChoice === 'venusaur' && compChoice === 'blastoise') {
+        result = 'win';
+        wins++;
+        winsDisplay.textContent = wins;
+    } else if (trainerChoice === 'blastoise' && compChoice === 'charizard') {
+        result = 'win';
+        wins++;
+        winsDisplay.textContent = wins;
+    } else if (trainerChoice === 'venusaur' && compChoice === 'charizard') {
+        result = 'lose';
+        losses++;
+        lossesDisplay.textContent = losses;
+    } else if (trainerChoice === 'blastoise' && compChoice === 'venusaur') {
+        result = 'lose';
+        losses++;
+        lossesDisplay.textContent = losses;
+    } else if (trainerChoice === 'charizard' && compChoice === 'blastoise') {
+        result = 'lose';
+        losses++;
+        lossesDisplay.textContent = losses;
+    }
+
+    return result;
+};
 
 /* Components */
 
@@ -27,25 +79,75 @@ const drawsDisplay = document.getElementById('draws-display');
 const char = document.getElementById('charizard');
 const blast = document.getElementById('blastoise');
 const ven = document.getElementById('venusaur');
+const compChar = document.getElementById('comp-char');
+const compBlast = document.getElementById('comp-blast');
+const compVen = document.getElementById('comp-ven');
 
 // display
 function displayChoices() {
-    char.classList.remove('hidden');
-    blast.classList.remove('hidden');
-    ven.classList.remove('hidden');
-
-    if (gameState === 'results') {
-        resultSection.classList.remove('hidden');
+    if (gameState === 'choose') {
+        resultSection.classList.add('hidden');
+        char.classList.remove('choose');
+        blast.classList.remove('choose');
+        ven.classList.remove('choose');
+        char.classList.remove('hidden');
+        blast.classList.remove('hidden');
+        ven.classList.remove('hidden');
+        compChar.classList.remove('hidden');
+        compBlast.classList.remove('hidden');
+        compVen.classList.remove('hidden');
     }
 }
 
 // event listeners
+char.addEventListener('click', () => {
+    char.classList.add('choose');
+    setTimeout(() => {
+        blast.classList.add('hidden');
+        ven.classList.add('hidden');
+    }, '500');
+    trainerChoice = 'charizard';
+    compChoice = getRandomItem(pokeChoices);
+    choosePokemon('charizard');
+    gameState = 'results';
+    displayResults();
+});
+
+blast.addEventListener('click', () => {
+    blast.classList.add('choose');
+    char.classList.add('hidden');
+    ven.classList.add('hidden');
+    choosePokemon('blastoise');
+    gameState = 'results';
+    displayResults();
+});
+ven.addEventListener('click', () => {
+    ven.classList.add('choose');
+    blast.classList.add('hidden');
+    char.classList.add('hidden');
+    choosePokemon('venusaur');
+    gameState = 'results';
+    displayResults();
+});
 
 // Results
 const resultSection = document.getElementById('results');
 const userPokemon = document.getElementById('user-pokemon');
 const compPokemon = document.getElementById('computer-pokemon');
 const battleResult = document.getElementById('battle-result');
+const playAgainButton = document.getElementById('play-again');
+
+playAgainButton.addEventListener('click', () => {
+    gameState = 'choose';
+    displayChoices();
+});
+
+function displayResults() {
+    if (gameState === 'results') {
+        resultSection.classList.remove('hidden');
+        // selectWinner();
+    }
+}
 
 /* Run page load code */
 loadPage();
